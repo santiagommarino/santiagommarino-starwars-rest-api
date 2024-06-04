80a151a8710a85b4cef9aa7d3e36b44e3fc726d4
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-06-04 20:39:40 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,17 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def return_people():
+
+    all_people = People.query.all()
+    all_people = list(map(lambda i: i.serialize(), all_people))
+    response_body = all_people
+
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,63 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    persons_name = db.Column(db.String(50), nullable=False)
+    height = db.Column(db.String(50), nullable=True)
+    mass = db.Column(db.String(50), nullable=True)
+    hair_color = db.Column(db.String(50), nullable=True)
+    skin_color = db.Column(db.String(50), nullable=True)
+    eye_color = db.Column(db.String(50), nullable=True)
+    birth_year = db.Column(db.String(50), nullable=True)
+    gender = db.Column(db.String(50), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.persons_name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+        }
+    
+class Planets(db.Model):
+    __tablename__ = "planets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    planets_name = db.Column(db.String(50), nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=True)
+    gravity = db.Column(db.Integer, nullable=True)
+    population= db.Column(db.Integer, nullable=True)
+    climate = db.Column(db.String(50), nullable=True)
+    terrain = db.Column(db.String(50), nullable=True)
+    surface_water = db.Column(db.Integer, nullable=True)
+    rotation_period = db.Column(db.Integer, nullable=True)
+    created = db.Column(db.String(50), nullable=True)
+    edited =  db.Column(db.Integer, nullable=True)
+    diameter = db.Column(db.Integer, nullable=True)
+    url = db.Column(db.String(50), nullable=True)
+
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planets_name": self.planets_name,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "rotation_period": self.rotation_period,
+            "created": self.created,
+            "edited": self.edited,
+            "diameter": self.diameter,
+            "url": self.url
+        }
